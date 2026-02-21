9b544032de29cb9487dd36b2d5d4a1b5df07d3cb
TagoAB <62874627+SubhiNajjar@users.noreply.github.com>
2026-02-21 21:10:43 +0100

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:5000/api", //'https://doctorbookingapi-production.up.railway.app/api',
+  baseURL: window.location.host.includes(
+    "doctorbookingclient-production.up.railway.app",
+  )
+    ? "https://doctorbookingapi-production.up.railway.app/api"
+    : "http://localhost:5000/api", //'',
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
